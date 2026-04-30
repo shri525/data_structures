8a578ed2d16f6ecfc64f9bbcde5753401818abb4
shreya <kaleshreya525@gmail.com>
2026-04-30 12:10:51 +0530

    linkedlistqueue.cpp file

---
+++ Exp8linkedlistqueue.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct node {
+    int data;
+    node* prev;
+    node* next;
+};
+
+int main() {
+    node *head = NULL, *temp, *newNode;
+    queue<int> q;
+    int choice, value, n;
+    char x;
+
+    do {
+        cout << "1. Create List\n";
+        cout << "2. Display List\n";
+        cout << "3. Reverse using Queue\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice) {
+
+            case 1: 
+            {
+                cout << "Enter number of elements: ";
+                cin >> n;
+
+                for(int i = 0; i < n; i++) {
+                    newNode = new node;
+                    cout << "Enter value: ";
+                    cin >> value;
+
+                    newNode->data = value;
+                    newNode->prev = NULL;
+                    newNode->next = NULL;
+
+                    if(head == NULL) {
+                        head = newNode;
+                    } else {
+                        temp = head;
+                        while(temp->next != NULL)
+                            temp = temp->next;
+
+                        temp->next = newNode;
+                        newNode->prev = temp;
+                    }
+                }
+                cout << "List Created\n";
+                break;
+            }
+
+            case 2:
+            {
+                if(head == NULL) {
+                    cout << "List is Empty\n";
+                } else {
+                    temp = head;
+                    cout << "List:\n";
+                    while(temp != NULL) {
+                        cout << temp->data << " ";
+                        temp = temp->next;
+                    }
+                    cout << endl;
+                }
+                break;
+            }
+
+            case 3:
+            {
+                if(head == NULL) {
+                    cout << "List is Empty\n";
+                } else {
+                    temp = head;
+
+                    while(temp != NULL) {
+                        q.push(temp->data);
+                        temp = temp->next;
+                    }
+
+                    int arr[100], i = 0;
+                    while(!q.empty()) {
+                        arr[i++] = q.front();
+                        q.pop();
+                    }
+                    temp = head;
+                    for(int j = i - 1; j >= 0; j--) {
+                        temp->data = arr[j];
+                        temp = temp->next;
+                    }
+
+                    cout << "List Reversed using Queue\n";
+                }
+                break;
+            }
+
+        cout << "Do you want to continue (y/n): ";
+        cin >> x;
+
+    } while(x == 'y' || x == 'Y');
+
+    return 0;
+    }
